8ff4f5c9d94fc01dc5fff8a82161303d82d6fa86
Abdullah Ashfaq <abdullahashfaq.ds@gmail.com>
2024-02-03 11:22:07 +0500

    Rendered Base App Dynamic Templates & URLs

--- base/views.py
+++ base/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn Python!'},
+    {'id': 2, 'name': 'Django Developers'},
+    {'id': 3, 'name': 'React Developers'},
+]
+
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for dic in rooms:
+        if dic['id'] == int(pk):
+            room = dic
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
